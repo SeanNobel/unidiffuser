8c8883878f4cf089cacb4234a4d1263a5c72dc48
SeanNobel <sensho19950503@gmail.com>
2024-01-26 21:50:39 +0900

    setup

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="unidiffuser",
+    packages=setuptools.find_packages(),
+    version="0.0.0",
+    author="Sensho Nobe",  # Originally Fan Bao
+    author_email="sean.y.nobel@gmail.com",
+    description="UniDiffuser",
+    url="https://github.com/SeanNobel/unidiffuser.git",
+    # classifiers=[
+    #     "Programming Language :: Python :: 3",
+    #     "License :: OSI Approved :: MIT License",
+    #     "Operating System :: OS Independent",
+    # ],
+    install_requires=[],
+    python_requires=">=3.6",  # developed on 3.9 / 3.10
+)
